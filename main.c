210643f0e6cf40c26d2a22f76ed8e04dfa47b277
JohanMadsen <johan@bloch-madsen.dk>
2019-01-15 16:41:45 +0100

    working version 1

--- main.c
+++ main.c
@@ -40,48 +40,60 @@ free_2d(double **A) {
     free(A);
 }
 
-double norm(double **m1, double**m2, int N){
+
+double jacobiIteration(double ** u, double ** uold,double ** f, double gridspacingsqr, int N){
+    double temp;
     double sum=0;
     for (int i = 1; i <N-1 ; ++i) {
         for (int j = 1; j <N-1 ; ++j) {
-            sum+=(m1[i][j]-m2[i][j])*(m1[i][j]-m2[i][j]);
+            temp=u[i][j];
+            u[i][j]=0.25*(uold[i][j-1]+uold[i][j+1]+uold[i-1][j]+uold[i+1][j]+f[i-1][j-1]*gridspacingsqr);
+            sum+=(u[i][j]-temp)*(u[i][j]-temp);
         }
     }
     return sqrt(sum);
 }
 
-void jacobiIteration(double ** u, double ** uold,double ** f, double gridspacingsqr, int N){
-    for (int i = 1; i <N-1 ; ++i) {
-        for (int j = 1; j <N-1 ; ++j) {
-            u[i][j]=0.25*(uold[i][j-1]+uold[i][j+1]+uold[i-1][j]+uold[i+1][j]+f[i][j]*gridspacingsqr);
-        }
-    }
-}
-
 void jacobi(int N, int kmax, double threshold,double ** u, double ** f,double gridspacingsqr){
     double ** uold=malloc_2d(N+2,N+2);
+    for (int i = 0; i <N+2 ; ++i) {
+        for (int j = 0; j <N+2 ; ++j) {
+            uold[i][j]=u[i][j];
+        }
+    }
     double d=DBL_MAX;
     int k=0;
     while(d>threshold && k<kmax){
         swap(&u,&uold);
-        jacobiIteration(u,uold,f,gridspacingsqr,N);
-        d=norm(u,uold,N);
+        d=jacobiIteration(u,uold,f,gridspacingsqr,N);
         k+=1;
-        printf("%f \n ",d);
+        printf("%d  ",k);
     }
     free_2d(uold);
 }
 
-void gaussIteration(double ** u,double ** f, double gridspacingsqr, int N){
+double gaussIteration(double ** u,double ** f, double gridspacingsqr, int N){
+    double sum=0;
+    double temp;
     for (int i = 1; i <N+1 ; ++i) {
         for (int j = 1; j <N+1 ; ++j) {
+            temp=u[i][j];
             u[i][j]=0.25*(u[i][j-1]+u[i][j+1]+u[i-1][j]+u[i+1][j]+f[i-1][j-1]*gridspacingsqr);
+            sum+=(u[i][j]-temp)*(u[i][j]-temp);
+
         }
     }
+    return sqrt(sum);
 }
 
-void gauss(int N, int kmax, double threshold,double ** u, double ** f){
-
+void gauss(int N, int kmax, double threshold,double ** u, double ** f,double gridspacingsqr){
+    double d=DBL_MAX;
+    int k=0;
+    while(d>threshold && k<kmax){
+        d=gaussIteration(u,f,gridspacingsqr,N);
+        k+=1;
+        printf("%d  ",k);
+    }
 }
 
 double ** generateF(int N, double gridspacing ){
@@ -98,17 +110,23 @@ double ** generateF(int N, double gridspacing ){
 }
 
 int main() {
-    int N=32;
-    int kmax=10000000;
+    int N=64;
+    int kmax=10000;
     double threshold=0.1;
-    double gridspacing=(double)2/N;
+    double gridspacing=(double)2/(N-1);
     double ** f = generateF(N,gridspacing);
     double ** u =malloc_2d(N+2,N+2);
     for (int i = 0; i <N+2 ; ++i) {
         for (int j = 0; j <N+2 ; ++j) {
-            u[i][j]=1;
+            if(i==0||i==N+1||j==N+1){
+                u[i][j]=20;
+            }
+            else{
+                u[i][j]=0;
+            }
         }
     }
     jacobi(N,kmax,threshold,u,f,gridspacing*gridspacing);
+    gauss(N,kmax,threshold,u,f,gridspacing*gridspacing);
     return 0;
 }
