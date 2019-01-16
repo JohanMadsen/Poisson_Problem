95bc5b0dfe33a8aa270ea94f2a42c3de4976dce8
Lasse Herskind <Nanochrome@users.noreply.github.com>
2019-01-16 13:08:00 +0100

    Loop blocking versions

--- iterator.c
+++ iterator.c
@@ -1,19 +1,61 @@
 #include <math.h>
 #include "iterator.h"
 
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
 double gaussIteration(double ***u, double ***f, double gridspacingsqr, int N) {
     double sum = 0;
     double temp;
+
     for (int i = 1; i < N + 1; ++i) {
         for (int j = 1; j < N + 1; ++j) {
             temp = (*u)[i][j];
-            (*u)[i][j] = 0.25 * ((*u)[i][j - 1] + (*u)[i][j + 1] + (*u)[i - 1][j] + (*u)[i + 1][j] + (*f)[i - 1][j - 1] * gridspacingsqr);
+            (*u)[i][j] = 0.25 * ((*u)[i][j - 1] + (*u)[i][j + 1] + (*u)[i - 1][j] + (*u)[i + 1][j] +
+                                 (*f)[i - 1][j - 1] * gridspacingsqr);
             sum += ((*u)[i][j] - temp) * ((*u)[i][j] - temp);
-
         }
     }
     return sqrt(sum);
 }
+
+double gaussIterationBlk(double ***u, double ***f, double gridspacingsqr, int N, int bs) {
+    double sum = 0;
+    double temp;
+
+    for (int i1 = 1; i1 < N + 1; i1 += bs) {
+        for (int j1 = 1; j1 < N + 1; j1 += bs) {
+            for (int i = i1; i < MIN(i1 + bs, N + 1); ++i) {
+                for (int j = j1; j < MIN(j1 + bs, N + 1); ++j) {
+                    temp = (*u)[i][j];
+                    (*u)[i][j] = 0.25 * ((*u)[i][j - 1] + (*u)[i][j + 1] + (*u)[i - 1][j] + (*u)[i + 1][j] +
+                                         (*f)[i - 1][j - 1] * gridspacingsqr);
+                    sum += ((*u)[i][j] - temp) * ((*u)[i][j] - temp);
+                }
+            }
+        }
+    }
+    return sqrt(sum);
+}
+
+double jacobiIterationBlk(double ***u, double ***uold, double ***f, double gridspacingsqr, int N, int bs) {
+    double sum = 0;
+    double temp;
+
+    for (int i1 = 1; i1 < N + 1; i1 += bs) {
+        for (int j1 = 1; j1 < N + 1; j1 += bs) {
+            for (int i = i1; i < MIN(i1 + bs, N + 1); ++i) {
+                for (int j = j1; j < MIN(j1 + bs, N + 1); ++j) {
+                    temp = (*u)[i][j];
+                    (*u)[i][j] = 0.25 * ((*uold)[i][j - 1] + (*uold)[i][j + 1] + (*uold)[i - 1][j] + (*uold)[i + 1][j] +
+                                         (*f)[i - 1][j - 1] * gridspacingsqr);
+                    sum += ((*u)[i][j] - temp) * ((*u)[i][j] - temp);
+                }
+            }
+        }
+    }
+    return sqrt(sum);
+}
+
 
 double jacobiIteration(double ***u, double ***uold, double ***f, double gridspacingsqr, int N) {
     double temp;
--- iterator.h
+++ iterator.h
@@ -2,6 +2,9 @@
 double gaussIteration(double ***u, double ***f, double gridspacingsqr, int N);
 double jacobiIteration(double ***u, double ***uold, double ***f, double gridspacingsqr, int N);
 double jacobiIteration2(double ***u, double ***uold, double ***f, double gridspacingsqr, int N);
+double gaussIterationBlk(double ***u, double ***f, double gridspacingsqr, int N, int bs);
+double jacobiIterationBlk(double ***u, double ***uold, double ***f, double gridspacingsqr, int N, int bs);
+
 #ifndef C_ITERATOR_H
 #define C_ITERATOR_H
 
