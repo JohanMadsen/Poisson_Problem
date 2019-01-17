09127137eb0f649b4299521fe5c9a8116ad0405a
Jacob Pjetursson <jacob@pjetursson.com>
2019-01-17 16:07:54 +0100

    simulate sh

--- simulate.sh
+++ simulate.sh
@@ -14,20 +14,36 @@ module load gcc/8.2.0
 #export OMP_WAIT_POLICY=
 
 mkdir -p sim
-rm -r sim/par_jacobi*.txt
+rm -r sim/par_jacobi_advanced*.txt
 
 for i in {1..16}
 do
 	export OMP_NUM_THREADS=1
-	./poisson.gcc jacobi $(($i*50)) 20000 0.0 >> sim/par_jacobi_1.txt
+	./poisson.gcc jacobi $(($i*50)) 20000 0.0 >> sim/par_jacobi_advanced_1.txt
 	export OMP_NUM_THREADS=2
-    ./poisson.gcc jacobi $(($i*50)) 20000 0.0 >> sim/par_jacobi_2.txt
+    ./poisson.gcc jacobi $(($i*50)) 20000 0.0 >> sim/par_jacobi_advanced_2.txt
     export OMP_NUM_THREADS=4
-    ./poisson.gcc jacobi $(($i*50)) 20000 0.0 >> sim/par_jacobi_4.txt
+    ./poisson.gcc jacobi $(($i*50)) 20000 0.0 >> sim/par_jacobi_advanced_4.txt
     export OMP_NUM_THREADS=8
-	./poisson.gcc jacobi $(($i*50)) 20000 0.0 >> sim/par_jacobi_8.txt
+	./poisson.gcc jacobi $(($i*50)) 20000 0.0 >> sim/par_jacobi_advanced_8.txt
 	export OMP_NUM_THREADS=12
-    ./poisson.gcc jacobi $(($i*50)) 20000 0.0 >> sim/par_jacobi_12.txt
+    ./poisson.gcc jacobi $(($i*50)) 20000 0.0 >> sim/par_jacobi_advanced_12.txt
 	export OMP_NUM_THREADS=16
-    ./poisson.gcc jacobi $(($i*50)) 20000 0.0 >> sim/par_jacobi_16.txt
+    ./poisson.gcc jacobi $(($i*50)) 20000 0.0 >> sim/par_jacobi_advanced_16.txt
+done
+
+for i in {9..24}
+do
+	export OMP_NUM_THREADS=1
+	./poisson.gcc jacobi $(($i*100)) 20000 0.0 >> sim/par_jacobi_advanced_1.txt
+	export OMP_NUM_THREADS=2
+    ./poisson.gcc jacobi $(($i*100)) 20000 0.0 >> sim/par_jacobi_advanced_2.txt
+    export OMP_NUM_THREADS=4
+    ./poisson.gcc jacobi $(($i*100)) 20000 0.0 >> sim/par_jacobi_advanced_4.txt
+    export OMP_NUM_THREADS=8
+	./poisson.gcc jacobi $(($i*100)) 20000 0.0 >> sim/par_jacobi_advanced_8.txt
+	export OMP_NUM_THREADS=12
+    ./poisson.gcc jacobi $(($i*100)) 20000 0.0 >> sim/par_jacobi_advanced_12.txt
+	export OMP_NUM_THREADS=16
+    ./poisson.gcc jacobi $(($i*100)) 20000 0.0 >> sim/par_jacobi_advanced_16.txt
 done
